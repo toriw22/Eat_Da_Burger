3558812b9813ea9be87f7df0837d1ea4985ebf7a
Tori White <toriw22@gmail.com>
2017-09-06 18:02:37 -0700

    sql files set up

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,11 @@
-.
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP,
+	PRIMARY KEY (id)	 
+)
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,7 @@
-.
+USE burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ("G Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("In-N-Out Double Double");
+
+INSERT INTO burgers (burger_name) VALUES ("Umami Burger");
